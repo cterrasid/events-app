c350c26b935561e31468feb4aebc3d471f93bc87
cterrasid <clarette.terrasi@gmail.com>
2019-10-28 23:16:20 +0100

    Create context, install axios  and get data

---
+++ src/context/ContextCategories.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+//Crear el context
+const ContextCategories = React.createContext();
+
+//Exportar el Consumer
+export const ConsumerCategories = ContextCategories.Consumer;
+
+class ProviderCategories extends Component {
+	token = 'LZB65BYVIO4435B6STTS';
+
+	state = { categories: [] };
+
+	componentDidMount() {
+		this.getCategories();
+	}
+
+	getCategories = async () => {
+		let url = `https://www.eventbriteapi.com/v3/categories/?token=${this.token}`;
+
+		let categories = await axios.get(url);
+
+		this.setState({
+			categories: categories.data.categories,
+		});
+	};
+
+	render() {
+		return (
+			<ContextCategories.Provider value={{ categories: this.state.categories }}>
+				{this.props.children}
+			</ContextCategories.Provider>
+		);
+	}
+}
+
+export default ProviderCategories;
